e5cc63f82905dc9cf934935b6badc6662fdec85b
z20ist21 <z20ist21@z00p3-11.SPSKNM>
2023-03-07 09:50:39 +0100

    uprava css a funkčnost storage

--- src/Products.tsx
+++ src/Products.tsx
@@ -4,34 +4,34 @@ import LandingPage from "./LandingPage";
 import { supabase } from "./supabase/supabaseClient";
 
 
-const Product = ( ) => {
-  useEffect (()=>{
+const Product = () => {
+  useEffect(() => {
     async function fetchproducts() {
       const res = await supabase
         .storage
         .from('productimages').getPublicUrl("hoodie.png")
-        console.log(res)
+      console.log(res)
     }
     fetchproducts()
-  },[])  
+  }, [])
   return (
-      <div>
-        
+    <div>
+
       <header>
-      <img style={{ width: 100, height: 100 }} src="src/img/jak.png" alt="j&k"/>
+        <img style={{ width: 100, height: 100 }} src="src/img/jak.png" alt="j&k" />
         <NavBar />
       </header>
-        <main>
-        <img src="src/img/hoodie.png" alt="Shrooms"/>
-        <h3>Black Hoodie</h3>
-        <p>20eka</p>
-        <img style={{ width: 450, height: 450 }} src="src/img/shirt.png" alt="Shrooms" />
-        <h3>Black Tričenko</h3>
-        <p>10eka</p>
-        </main>
-      </div>
-    );
-  };
+      <main>
+        <div className="image-grid">
+          <img src="https://wemfvovctkoappoaqcby.supabase.co/storage/v1/object/public/productimages/hoodie.png" alt="obrazok 1" />
+          <img src="https://wemfvovctkoappoaqcby.supabase.co/storage/v1/object/public/productimages/shirt2.png" alt="obrazok 2" />
+          <img src="https://wemfvovctkoappoaqcby.supabase.co/storage/v1/object/public/productimages/socks2" alt="obrazok 3" />
+          {/* Tu môžeš pridať ďalšie obrázky */}
+        </div>
+      </main>
+    </div>
+  );
+};
 
-  
+
 export default Product;
