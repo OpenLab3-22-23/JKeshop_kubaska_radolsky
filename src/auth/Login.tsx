351fdac1f70727b21bb6e76f8c66c28b25566e86
Grazog666 <90481241+Grazog666@users.noreply.github.com>
2023-03-20 22:37:11 +0100

    Login css
    pridanie csska pre login stranku

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -29,6 +29,12 @@ export default function LogIn() {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           className="w-64 my-2"
+          style={{ 
+            border: "2px solid gray", 
+            borderRadius: "5px", 
+            padding: "10px",
+            fontSize: "16px",
+          }}
         />
         <input
           id="password"
@@ -37,15 +43,29 @@ export default function LogIn() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           className="w-64 my-2"
+          style={{ 
+            border: "2px solid gray", 
+            borderRadius: "5px", 
+            padding: "10px",
+            fontSize: "16px",
+          }}
         />
         <input
           type="submit"
           value="Log In"
           className="w-64 rounded-full bg-sky-500/50 px-2 py-1 my-4"
+          style={{ 
+            backgroundColor: "skyblue",
+            border: "none",
+            color: "white",
+            fontWeight: "bold",
+            fontSize: "18px",
+            cursor: "pointer",
+          }}
         />
-        <p className="mt-5">
+        <p className="mt-5" style={{ fontSize: "16px" }}>
           Don't have an account?{" "}
-          <Link to="/signup" className="text-emerald-300">
+          <Link to="/signup" className="text-emerald-300" style={{ textDecoration: "none" }}>
             Sign Up
           </Link>
         </p>
